15eac0d923ad806d213b3551621e8ee5f6c3661e
wangzhigang <wangzhigang@chunyu.me>
2016-11-14 20:57:50 +0800

    添加1.x版本rxJava支持  封装了下载功能

--- app/src/main/java/retrofit2/OkHttp.java
+++ app/src/main/java/retrofit2/OkHttp.java
@@ -9,7 +9,7 @@ import okhttp3.OkHttpClient;
 import okhttp3.logging.HttpLoggingInterceptor;
 
 /**
- * Created by Aaron on 2016/11/11.
+ * Created by Aaron Wang on 2016/11/11.
  */
 public enum OkHttp {
     INSTANCE;
